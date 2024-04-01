1fb120a0590e5a462c2bd4979f37af78f69ec04f
drimerdev <miguelvicente044@proton.me>
2024-04-01 17:38:42 -0300

    add more things

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+// Mig's vCPU
+// version => 0.0.1 alpha #1
+// env => nodejs
+// script => javascript
+// CPU(s) => null
+
 var usr
 var clock
 var cpu
-var emu 
+var emu
+var initalize
 
 const clock = x
 
@@ -11,4 +18,18 @@ function _init() {
 
 function _conf() {
     // cpu main configuration
+}
+
+function _emu() {
+    // cpu emulation manager
+	const { program } = require('commander);
+	program
+	 .name('Miguel´s vCPU manager')
+	 .version('0.0.1')
+
+	// start cpu command
+	program.command('start')
+	.description('Start vCPU emulation manager')
+
+	program.parse();
 }
